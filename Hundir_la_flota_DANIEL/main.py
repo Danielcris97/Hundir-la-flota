49033fea5b351debf987262a0803fc212ffde181
Danielcris97 <da.cristobal.ss@gmail.com>
2025-05-08 20:57:28 +0200

    disp e impr

--- Hundir_la_flota_DANIEL/main.py
+++ Hundir_la_flota_DANIEL/main.py
@@ -1,6 +1,7 @@
 import utils
 import variables as vr
 import time
+import numpy as np
 """print("INICIANDO JUEGO...")
 time.sleep(1)
 print("Bienvenido al juego: Hundir la flota")
@@ -31,11 +32,29 @@ def print_tableros():
     #time.sleep(1)#
     print(tablero_jugador_disparos)
     #time.sleep(2)#
+def juego():
+    contar_OJ = np.count_nonzero(tablero_jugador_barcos == "O")
+    contar_OR = np.count_nonzero(tablero_rival_barcos == "O")
+    if contar_OJ <0:
+        print("ganaste")
+    if contar_OR <0:
+        print("perdiste")    
+    while contar_OJ> 0 and contar_OR > 0:
+        if utils.turno_jugador == 1:
+            utils.disparo_rival()
+            utils.disparo()
+            juego(juego)
+        if utils.turno_jugador == 0:
+            utils.disparo()
+            utils.disparo_rival()
 
+utils.colocar_barcos(tablero_jugador_barcos, vr.barcos_jugador)
+utils.colocar_barcos(tablero_rival_barcos, vr.barcos_rival)
 print_tableros()
-colocar_barcos_jugador = utils.colocar_barcos(tablero_jugador_barcos, vr.barcos_jugador)
-colocar_barcos_rival = utils.colocar_barcos(tablero_rival_barcos, vr.barcos_rival)
-
 disparo_jugador = utils.disparo(tablero_rival_barcos,tablero_jugador_disparos)
 disparo_rival = utils.disparo_rival(tablero_jugador_barcos, tablero_rival_disparos)
 print_tableros()
+
+
+
+
--- Hundir_la_flota_DANIEL/utils.py
+++ Hundir_la_flota_DANIEL/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import variables as vr
+
+turno_jugador = np.random.random_integers(0, 1)
 def crear_tablero():
     tablero = np.full((10,10), "_")
     return tablero
@@ -8,8 +10,7 @@ def crear_tablero():
 def colocar_barcos(tablero, barcos):
     for  i in barcos:
         for j in i:
-            tablero[j[0], j[1]] = "O"
-        
+            tablero[j[0], j[1]] = "O" 
     return tablero
 
 def disparo(tablero, tablero2):
@@ -47,7 +48,7 @@ def disparo_rival(tablero,tablero2):
         o_count = np.count_nonzero(tablero == "O")
         print("¡¡¡IMPACTOOOOO DEL RIVAL!!!")
         if o_count == o_count1 - 1:
-            disparo_rival(tablero,tablero2)
+            disparo_rival(tablero,tablero2)  
     elif tablero[fila,columna] == "#" or tablero[fila, columna] == "X":
         print("Posiccion ya seleccionada por el rival")
         disparo_rival(tablero,tablero2)
@@ -57,4 +58,11 @@ def disparo_rival(tablero,tablero2):
         print("FALLO DEL RIVAL :)")
     return tablero
 
-    
+def turno_inicial():
+    if turno_jugador == 1:
+        disparo(tablero_rival_barcos,tablero_jugador_disparos)
+    if turno_jugador == 0:
+        disparo_rival(tablero_jugador_barcos, tablero_rival_disparos)
+
+
+
