dd2fb894bce18dc2d1c1a82a451844360ad0d7de
Danielcris97 <da.cristobal.ss@gmail.com>
2025-05-08 22:49:22 +0200

    final 22:49

--- Hundir_la_flota_DANIEL/main.py
+++ Hundir_la_flota_DANIEL/main.py
@@ -2,11 +2,11 @@ import utils
 import variables as vr
 import time
 import numpy as np
-"""print("INICIANDO JUEGO...")
+print("INICIANDO JUEGO...")
 time.sleep(1)
 print("Bienvenido al juego: Hundir la flota")
 time.sleep(2)
-"""
+
 
 tablero_rival_barcos = utils.crear_tablero()    # MOSTRAR BARCOS DEL RIVAL Y LOS DISPAROS DEL JUGADOR.
 tablero_rival_disparos = utils.crear_tablero()  # MOSTRAR DISPAROS DEL RIVAL.
@@ -16,22 +16,22 @@ tablero_jugador_disparos = utils.crear_tablero()    # MOSTRAR DISPAROS DEL JUGAD
 
 def print_tableros():
     print("TABLERO DONDE SE MUESTRAN LOS BARCOS DEL RIVAL Y LOS DISPAROS DEL JUGADOR.")
-    #time.sleep(1)#
+    time.sleep(0.5)
     print(tablero_rival_barcos)
-    #time.sleep(2)#
+    time.sleep(2)
     print("TABLERO DONDE SE MUESTRAN LOS DISPAROS DEL RIVAL")
-    #time.sleep(1)#
+    time.sleep(0.5)
     print(tablero_rival_disparos)
     print("___________________________________")
-    #time.sleep(2)#
+    time.sleep(2)
     print("TABLERO DONDE SE MUESTRA LOS BARCOS DEL JUGADOR Y LOS DISPAROS DEL RIVAL")  
-    #time.sleep(1)#
+    time.sleep(0.5)
     print(tablero_jugador_barcos)
-    #time.sleep(2)#
+    time.sleep(2)
     print("TABLERO DONDE SE MUESTRAN LOS DISPAROS DEL JUGADOR.")
-    #time.sleep(1)#
+    time.sleep(0.5)
     print(tablero_jugador_disparos)
-    #time.sleep(2)#
+    time.sleep(2)
 def juego():
      while True:
         if utils.turno_jugador == 1:
@@ -57,7 +57,7 @@ def juego():
             print("Ahora el rival dispara")
             utils.disparo_rival(tablero_jugador_barcos, tablero_rival_disparos)
             if np.count_nonzero(tablero_jugador_barcos == "O") == 0:
-                print("¡La máquina ha ganado!")
+                print("¡El rival ha ganado!")
                 break
 
         print_tableros()
