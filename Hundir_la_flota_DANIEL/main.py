be0486fd6dec881a3feaf296c2c60ba6fae42a67
Danielcris97 <da.cristobal.ss@gmail.com>
2025-05-09 10:46:00 +0200

    explicaciones

--- Hundir_la_flota_DANIEL/main.py
+++ Hundir_la_flota_DANIEL/main.py
@@ -36,19 +36,19 @@ def juego():
      while True:
         if vr.turno_jugador == 1: #si el random es 1 inicia el rival
             print("\nTurno del RIVAL")
-            utils.disparo_rival(tablero_jugador_barcos, tablero_rival_disparos)
-            if np.count_nonzero(tablero_jugador_barcos == "O") == 0: #contar los barcos
+            utils.disparo_rival(tablero_jugador_barcos, tablero_rival_disparos) #rival dispara en tablero jugador y guarda su disparo
+            if np.count_nonzero(tablero_jugador_barcos == "O") == 0: #contar los barcos, si es 0 pierde
                 print("¡El rival ha ganado! :(")
                 break
 
-            print("Ahora es tu turno")
+            print("Ahora es tu turno") #cambio de turno
             utils.disparo(tablero_rival_barcos, tablero_jugador_disparos)
-            if np.count_nonzero(tablero_rival_barcos == "O") == 0:
+            if np.count_nonzero(tablero_rival_barcos == "O") == 0: #si el rival no tiene barco has ganado
                 print("¡Tú has ganado! ¡Felicidades! :)")
                 break
 
         else:
-            print("\nTu turno!")
+            print("\nTu turno!") #si el random es 0 inicia el jugador
             utils.disparo(tablero_rival_barcos, tablero_jugador_disparos)
             if np.count_nonzero(tablero_rival_barcos == "O") == 0:
                 print("¡Tú has ganado! ¡Felicidades! :)")
@@ -61,7 +61,7 @@ def juego():
                 break
 
         print_tableros()
-
+#inicio del juego, colocar barcos y tableros vacios
 utils.colocar_barcos(tablero_jugador_barcos, vr.barcos_jugador)
 utils.colocar_barcos(tablero_rival_barcos, vr.barcos_rival)
 print_tableros()
--- Hundir_la_flota_DANIEL/utils.py
+++ Hundir_la_flota_DANIEL/utils.py
@@ -6,7 +6,7 @@ def crear_tablero():
     return tablero
 
 
-def colocar_barcos(tablero, barcos):
+def colocar_barcos(tablero, barcos): 
     for  i in barcos:
         for j in i:
             tablero[j[0], j[1]] = "O" 
@@ -27,22 +27,22 @@ def disparo(tablero, tablero2): #tablero 2 es el tablero de disparos
         print("¡¡¡IMPACTOOOOO!!!")
         o_count = np.count_nonzero(tablero == "O")
 
-        if o_count > 0 and o_count == o_count1 - 1:
+        if o_count > 0 and o_count == o_count1 - 1: #si el conteo de barcos disminuye significa que acertó y repite turno.
             return disparo(tablero, tablero2)  # solo repite si aún hay barcos
 
     elif tablero[fila, columna] in ["#", "X"]:
-        print("Posiccion ya seleccionada")
+        print("Posiccion ya seleccionada") #posición ya seleccionada, repite turno
         return disparo(tablero, tablero2)
 
     else:
-        tablero[fila, columna] = "#"
+        tablero[fila, columna] = "#" #si no hay acierto ni disparos
         tablero2[fila, columna] = "#"
         print("FALLASTE :)")
 
     return tablero
 
 
-def disparo_rival(tablero, tablero2):
+def disparo_rival(tablero, tablero2): #selecciona aleatoriamente fila y columna
     fila = np.random.randint(0, 10)
     columna = np.random.randint(0, 10)
     o_count1 = np.count_nonzero(tablero == "O")
@@ -50,10 +50,10 @@ def disparo_rival(tablero, tablero2):
     if tablero[fila, columna] == "O":
         tablero[fila, columna] = "X"
         tablero2[fila, columna] = "X"
-        o_count = np.count_nonzero(tablero == "O")
+        o_count = np.count_nonzero(tablero == "O") #verifica si hay acierto
         print("¡¡¡IMPACTOOOOO DEL RIVAL!!!")
 
-        if o_count > 0 and o_count == o_count1 - 1:
+        if o_count > 0 and o_count == o_count1 - 1: #si hay barcos aún y disminuye en 1 repite turno
             return disparo_rival(tablero, tablero2)
 
     elif tablero[fila, columna] in ["#", "X"]:
